431032114ecae962c3245e7326618f417a803b78
aleksvvv <adm-shop-001@yandex.ru>
2022-11-04 19:55:54 +0300

    Lesson2. 04.11.22

---
+++ app/src/main/java/com/example/compositionsumin/presentation/GameFinishedFragment.kt
@@ -0,0 +1,30 @@
+package com.example.compositionsumin.presentation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.compositionsumin.R
+import com.example.compositionsumin.databinding.FragmentGameBinding
+import com.example.compositionsumin.databinding.FragmentGameFinishedBinding
+
+class GameFinishedFragment : Fragment() {
+
+    private var _binding: FragmentGameFinishedBinding? = null
+    private val binding: FragmentGameFinishedBinding
+        get() = _binding ?: throw RuntimeException("FragmentGameBinding == null")
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentGameFinishedBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
